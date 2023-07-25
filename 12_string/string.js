528f9d9414f561d2f3637261cfa1f2184d1c5867
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-25 16:26:16 +0000

    string methods

---
+++ 12_string/string.js
@@ -0,0 +1,17 @@
+// let aString = new String ('ankit mishra')
+// console.log (aString)
+
+// const repo = 5
+// const name = "ankit misra"
+
+// console.log(`there are repo ${repo} and anme is ${name}`)
+
+
+let gameName = "ankit mishra"
+
+// console.log(gameName[3])
+
+// console.log(gameName.__proto__)
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
