92e354aa30494eefe64eef51ad8b5e288f9b40c0
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-26 03:22:38 +0000

    math modules

---
+++ Learn_java_Script/12_NumberAndMath/math.js
@@ -0,0 +1,30 @@
+//math module==================================
+
+// console.log(Math); 
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.6666));
+
+// console.log(Math.ceil(4.2369))
+
+// console.log(Math.floor(4.6369));
+
+// console.log(Math.min(4,6,8,69))
+
+// console.log(Math.max(4,6,8,69))
+
+// console.log(Math.random());
+
+// console.log(Math.random()*10+1)
+
+// console.log(Math.floor(Math.random()*10+1));
+
+
+// if min and max are already diffine
+
+let min = 10;
+let max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
