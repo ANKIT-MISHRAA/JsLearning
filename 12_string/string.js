acdc77b711e782420a40a772552c7e2be207ca49
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-26 02:22:40 +0000

    some string methods

--- 12_string/string.js
+++ 12_string/string.js
@@ -13,5 +13,28 @@ let gameName = "ankit mishra"
 
 // console.log(gameName.__proto__)
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(4))
+// console.log(gameName."indexOf"('r'))
+
+
+let anothervalue = "jai shri ram"
+// console.log(anothervalue.substring(4,8))
+
+// console.log(anothervalue.slice(0,4))
+
+let newString = "    hare krishna    "
+
+// console.log(newString)
+// console.log(newString.trim())
+
+
+let url = "https://www.harekrishna%20hare%20kirshna"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hare'))
+
+console.log(url.split('%20'))
