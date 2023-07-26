ef5483e8cdc672153de032a635abe0729e8c4aed
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-26 02:49:21 +0000

    Number methods

---
+++ Learn_java_Script/12_NumberAndMath/number.js
@@ -0,0 +1,16 @@
+// number diffine and methods
+
+let balance = new Number('200')
+
+// console.log(balance);
+
+newString = balance.toString();
+// console.log(typeof(newString))
+
+// console.log(balance.toFixed(2))
+
+let aNumber = 23.336846
+// console.log(aNumber.toPrecision(3))
+
+let bankBalance = new Number (1000000)
+console.log(bankBalance.toLocaleString('en-In'))
