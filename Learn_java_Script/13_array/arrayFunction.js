ad1768458836cfd2670f4831461a1b782dd4710e
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-28 07:54:53 +0000

    Array methods

---
+++ Learn_java_Script/13_array/arrayFunction.js
@@ -0,0 +1,43 @@
+const marvelHeros = ["spiderman","ironman","thor"]
+const dcHeros = ["suparman","batman","flash"]
+
+// marvelHeros.push(dcHeros)
+// // console.log(marvelHeros)
+
+// console.log(marvelHeros[3][1])
+
+
+// let allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros)
+
+////    when multiple arrayes.....use spred operator
+
+// let allHeros = [...marvelHeros, ...dcHeros]
+// console.log(allHeros)
+
+
+// let anotherArray = [1,3,4,5,[3,5[6,12,45,12,34,56]]]
+
+// const realAnotherArray = anotherArray.flat(Infinity);
+// console.log(anotherArray)
+
+// let arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+// arr4.flat(Infinity);
+
+// console.log(arr4)
+
+// console.log(marvelHeros)
+// console.log(Array.isArray(marvelHeros))
+// console.log(Array.isArray("ironman"))
+
+// console.log(Array.from("ankit"))
+// console.log(Array.from({name:"ankit"}))
+
+let arr1 = 300
+let arr2 = 400
+let arr3 = 500
+
+console.log(Array.of(arr1,arr2,arr3))
+
+
+
