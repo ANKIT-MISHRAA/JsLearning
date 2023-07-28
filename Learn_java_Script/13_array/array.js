2e78b29049267e80e988c5df4afbca5c9d5cba59
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-28 06:43:46 +0000

    array methods

--- Learn_java_Script/13_array/array.js
+++ Learn_java_Script/13_array/array.js
@@ -1,9 +1,41 @@
 // array in javascript
 
 
-let arr = [1,2,3,4]
+// let arr = [1,2,3,4]
 
-let arr2 = new array (1,2,3,4,5);
+let arr2 = new Array(1,2,3,4,5)
 
-console.log(arr);
-console.log(arr2);
+// console.log(arr);
+// console.log(arr2);
+
+
+//  array methods +=====================
+
+// arr2.push(11)
+// console.log(arr2)
+
+// arr2.pop();
+// console.log(arr2)
+
+// arr2.unshift(2)
+// console.log(arr2)
+
+// arr2.shift()
+// console.log(arr2)
+// arr2.shift()
+// console.log(arr2)
+
+
+// console.log(arr2.includes(4));
+
+// console.log(arr2.indexOf(5))
+
+let theArray =  [1,2,4,5,6,6]
+
+const newArray = theArray.join()
+
+console.log(newArray);
+console.log(typeof(newArray))
+
+
+
