77bbbe8f57ff3b6de0927b95da16aad824dbc176
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-28 08:29:39 +0000

    DAte time methods

---
+++ Learn_java_Script/15_Dat_eandTime/dateTime.js
@@ -0,0 +1,17 @@
+//date and time
+
+// let mydate = new Date()
+
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+
+// console.log(typeof mydate);
+
+let myCreatedDate = new Date (2023,0,23,6,4,9)
+console.log(myCreatedDate);
+console.log(myCreatedDate.toString());
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+
