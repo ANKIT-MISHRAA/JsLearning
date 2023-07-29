c73fa8fbb87c2c6104806355373002f2350a01a9
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-29 08:19:49 +0000

    function rest operator

---
+++ 19_function/functoin2.js
@@ -0,0 +1,53 @@
+// rest operator
+
+
+// function calculateCartPraice(num1){
+//     return(num1)
+
+// }
+
+// console.log(calculateCartPraice(200,400,600))
+
+
+
+
+// function calculateCartPraice(...num1){
+//         return(num1)
+    
+//     }
+    
+//     console.log(calculateCartPraice(200,400,600))
+
+
+// const user ={
+//     userName : "hari mishra" ,
+//     price : 199
+// }
+
+// function handelObjct (anyobject){
+//     console.log(`user name is ${anyobject.userName} and price is ${anyobject.price}`)
+// }
+
+// handelObjct(user)
+
+
+// handelObjct({
+//     userName : "ravi mishra" ,
+//     price : 200
+// })
+
+
+
+//// function with arrya
+
+
+
+// const myNewArray = new Array (200,400,600,2000)
+
+function returnSecondArray (anyArray){
+        return anyArray[1]
+}
+
+
+// console.log(returnSecondArray(myNewArray))
+console.log(returnSecondArray([200,866,400,600,2000]))
