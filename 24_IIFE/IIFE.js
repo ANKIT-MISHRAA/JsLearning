ea1dc76d5c97853a966af808fe0e9474ac7604bb
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-07-29 14:08:47 +0000

    iife

---
+++ 24_IIFE/IIFE.js
@@ -0,0 +1,14 @@
+// function chai (){
+//     console.log(`db connected`)
+// }
+
+// chai()
+
+
+// (function chai (){
+//     console.log(`db connected`)
+// })()
+
+// (()=>{console.log(`DB connected`)})()
+
+((name)=>{console.log( name," DB connected")})("ankit")
