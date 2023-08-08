216b42e4184f80acff1a6e7c83e636c5f6393302
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-08-08 09:36:50 +0000

    for loop

---
+++ 27_forLoopWithBreak/for.js
@@ -0,0 +1,27 @@
+// for(let i=0 ; i<=10 ; i++){
+//     let eliment = i ;
+//     console.log(eliment);
+//     if(i==5){
+//         console.log("5 is a best number");
+//     }
+//     console.log(eliment);
+// }
+
+
+// for (let i = 0 ; i<=10 ; i++){
+//     console.log(`for outer loop value ${i}`);
+//     for(let j = 0 ; j<=10 ; j++){ 
+//     // console.log(`for outer loop value ${i} and inner loops value ${j}`);
+//     console.log(i + "*" + j + "=" + i*j );
+//     }
+// }
+
+
+let myArray = ["superman" ,"batman" ,"ironman"]
+
+for (let i = 0 ; i < myArray.length ; i++){
+    let eliment = myArray[i]
+    console.log(eliment)
+}
+
+
