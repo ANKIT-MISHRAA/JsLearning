0e581ba68ed51462ed24c4e396376e77611249af
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-08-08 07:19:07 +0000

    conrolFlow

---
+++ 26_ControlFlow/ControFlow.js
@@ -0,0 +1,74 @@
+/* ef else else if
+
+if(){
+
+}else{
+
+}
+
+
+if(){
+
+}else if(){
+
+}else{
+
+}
+
+
+swich  case
+
+
+let val = 3
+
+switch(val){
+case 1:  
+    console.log("mondey")
+    break;
+case 2:
+    console.log("tuseday")
+    break;
+case 3:
+    console.log("wednesday")
+    break;
+default:
+    console.log("fun day")
+    break;
+ }
+ */
+
+
+ //check empety array
+
+//  let userEmail = [3, 3]
+//   if (userEmail.length === 0){
+//     console.log("empety array")
+//   }else{
+//     console.log("array not empety");
+//   }
+
+
+// check empety object
+ 
+// let obj = {dd:"ram"}
+//  if (Object.keys(obj).length==0){
+//     console.log("object is empety");
+//  }else{
+//     console.log("object in not empety");
+//  }
+
+
+//Nulish conlescing Operator (??) null undifined
+
+let val1;
+val1 = 5 ?? 10 
+
+
+
+
+
+
+
+
+
+
