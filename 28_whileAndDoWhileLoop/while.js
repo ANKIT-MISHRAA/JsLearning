33929422ee65b135cf097736604fe395073dbc85
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-08-08 10:42:33 +0000

    while loop

---
+++ 28_whileAndDoWhileLoop/while.js
@@ -0,0 +1,15 @@
+// let index = 0 
+// while(index < 20){
+//     console.log(`the value of index is ${index}`);
+//     index++
+// }
+
+
+// let arr = ['superman','spiderman','thor','shaktiman']
+// let i = 0;
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++
+// }
+
+
