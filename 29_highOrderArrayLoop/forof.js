b8377470ffbd28a61a99374e5b5ff52ad627d45c
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-08-08 11:11:11 +0000

    for of on map and object

--- 29_highOrderArrayLoop/forof.js
+++ 29_highOrderArrayLoop/forof.js
@@ -10,3 +10,31 @@ const greetig = "hello world!"
 for(const greet in greetig){
     console.log(`the each value is ${greetig[greet]}`);
 }
+
+
+
+
+
+
+// const map = new Map()
+
+// map.set('in','india')
+// map.set('usa','united states of america')
+// map.set('fr','france')
+// map.set('in','india')
+
+// console.log(map);
+// for(const [key,value] of map){
+//     console.log(key,":=",value );
+// }
+
+let myObj = {
+    game1 : "gta",
+    game2 : "nfs"
+}
+
+for(const key of myObj){
+    console.log(key);
+}
+
+///it gives an error myObj is not itrateble
