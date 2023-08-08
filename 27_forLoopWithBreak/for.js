dbe03449a75ad39c6d4cebd6c54bd0b5e3b7fd6b
ANKIT MISHRA <115156311+ANKIT-MISHRAA@users.noreply.github.com>
2023-08-08 09:45:03 +0000

    for loop with break and continue

--- 27_forLoopWithBreak/for.js
+++ 27_forLoopWithBreak/for.js
@@ -17,11 +17,30 @@
 // }
 
 
-let myArray = ["superman" ,"batman" ,"ironman"]
+// let myArray = ["superman" ,"batman" ,"ironman"]
 
-for (let i = 0 ; i < myArray.length ; i++){
-    let eliment = myArray[i]
-    console.log(eliment)
+// for (let i = 0 ; i < myArray.length ; i++){
+//     let eliment = myArray[i]
+//     console.log(eliment)
+// }
+
+
+// break and continue
+
+
+for (let i=0 ; i<= 20 ; i++){
+    if(i == 5){
+        console.log(`detected 5`);
+        break;
+    }
+    console.log(`value of i is ${i}`)
 }
 
 
+for (let index=0 ; index<20 ; index++){
+    if(index == 5){
+        console.log(`5 is ditected`);
+        continue;
+    }
+    console.log(`the value of index is ${index} `);
+}
